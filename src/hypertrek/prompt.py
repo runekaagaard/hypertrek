453407d5e40be34d5e44160ddb6a7d7850fb6a09
Rune Kaagaard <rumi.kg@gmail.com>
2023-12-20 22:56:29 +0100

    I'm sure the reason I'm building a cli interface for django forms will reveal itself in due time!

--- src/hypertrek/prompt.py
+++ src/hypertrek/prompt.py
@@ -4,8 +4,10 @@ from prompt_toolkit.input.defaults import create_input
 from prompt_toolkit.output.defaults import create_output
 from prompt_toolkit.shortcuts import PromptSession
 from prompt_toolkit.validation import Validator, ValidationError
+from prompt_toolkit.completion import FuzzyWordCompleter
 
-def prompt_value(label, value_type=str, max_length=None, multiline=False, session=None):
+def prompt_value(label, default=None, value_type=str, max_length=None, multiline=False, choices=None,
+                 session=None):
     class Validate(Validator):
         def validate(self, document):
             try:
@@ -20,7 +22,14 @@ def prompt_value(label, value_type=str, max_length=None, multiline=False, sessio
 
     prmpt = session.prompt if session else prompt
     prompt_text = f"{label}: "
-    return value_type(prmpt(prompt_text, validator=Validate(), multiline=multiline, mouse_support=True))
+
+    completer = None
+    if choices:
+        completer = FuzzyWordCompleter([f"{k}: {v}" for k, v in choices])
+
+    return value_type(
+        prmpt(prompt_text, validator=Validate(), multiline=multiline, mouse_support=True, default=default,
+              completer=completer))
 
 test_cases = [
     ["name", str, 8, False],
@@ -51,19 +60,45 @@ class PocForm(forms.Form):
     options = forms.ChoiceField(choices=[("a", "A"), ("b", "B"), ("c", "C")], label="Which one?",
                                 help_text="Choose one, god dammit!")
     numb = forms.IntegerField()
+    num_float = forms.FloatField()
 
     def clean(self):
         if self.cleaned_data.get("options", None) == "b":
             raise ValidationError("NEVER choose b!")
 
-def prompt_field(field):
+def prompt_field(form, field_name):
+    field = form.fields[field_name]
     input_type = getattr(field.widget, "input_type", None)
-    print(input_type)
+    value_type = {"number": int, "text": str}.get(input_type, str)
+    if value_type is int:
+        step = field.widget.attrs.get('step', '1')
+        try:
+            if step == "any" or int(step) != float(step):
+                value_type = float
+        except ValueError:
+            pass
 
-form = PocForm(data=dict(numb=999))
-for field in form.fields.values():
-    prompt_field(field)
+    if form.is_bound:
+        default = form[field_name].field.bound_data(form[field_name].data, form.initial.get(field_name))
+    else:
+        default = form.initial[field_name]
+
+    prompt_value(
+        field_name,
+        str(default) if default is not None else "",
+        value_type=value_type,
+        max_length=getattr(field, "max_length", None),
+        multiline=isinstance(field.widget, forms.Textarea),
+        choices=getattr(field, "choices", None),
+    )
+
+form = PocForm(data=dict(numb=999), initial=dict(name="poop"))
+# form.is_valid()
+for field_name in form.fields.keys():
+    prompt_field(form, field_name)
 
 # form.is_valid()
 # field_value = prompt_field(form, 'numb')
 # print(field_value)
+
+# promt_value = prompt_value("name", "Baby Boy", str, 16)
