5de61cdb9ab8d07b13e184c174947e51570149ae
Rune Kaagaard <rumi.kg@gmail.com>
2023-12-20 21:37:55 +0100

    work

--- src/hypertrek/prompt.py
+++ src/hypertrek/prompt.py
@@ -5,36 +5,7 @@ from prompt_toolkit.output.defaults import create_output
 from prompt_toolkit.shortcuts import PromptSession
 from prompt_toolkit.validation import Validator, ValidationError
 
-class DjangoFieldValidator(Validator):
-    def __init__(self, django_field):
-        self.django_field = django_field
-
-    def validate(self, document):
-        text = document.text
-        # Perform type-specific validation
-        if isinstance(self.django_field, forms.IntegerField):
-            if text and not text.isdigit():
-                raise ValidationError(message="This field requires a valid integer.", cursor_position=len(text))
-
-def prompt_field(form, field_name, session=None):
-    field = form.fields[field_name]
-    current_value = form.cleaned_data.get(field_name, field.initial) if form.is_bound else field.initial
-    prompt_text = f"{field.label if field.label else field_name} (current: {current_value}): "
-
-    # Create a validator for the field
-    validator = DjangoFieldValidator(field)
-
-    # Multiline for Textarea
-    multiline = isinstance(field.widget, forms.Textarea)
-
-    if session:
-        # Use provided session for history
-        return session.prompt(prompt_text, multiline=multiline, validator=validator)
-    else:
-        # Prompt without a session
-        return prompt(prompt_text, multiline=multiline, validator=validator, mouse_support=True)
-
-def prompt_value(label, value_current, value_type=str, max_length=None, multiline=False, session=None):
+def prompt_value(label, value_type=str, max_length=None, multiline=False, session=None):
     class Validate(Validator):
         def validate(self, document):
             try:
@@ -48,28 +19,30 @@ def prompt_value(label, value_current, value_type=str, max_length=None, multilin
                     raise ValidationError(message=f"Value must be {max_length} characters or less")
 
     prmpt = session.prompt if session else prompt
-    prompt_text = f"{label} ({value_current}): "
+    prompt_text = f"{label}: "
     return value_type(prmpt(prompt_text, validator=Validate(), multiline=multiline, mouse_support=True))
 
 test_cases = [
-    ["Enter your name", "John Doe", str, 8, False],
-    ["Enter a description", "This is line 1.\nThis is line 2.", str, None, True],
-    ["Enter your age", "30", int, None, False],
-    ["Enter a float value", "3.14", float, None, False],
+    ["name", str, 8, False],
+    ["description", str, None, True],
+    ["age", int, None, False],
+    ["temperatur", float, None, False],
 ]
 
-# Iterate over the test cases and call prompt_value for each
-for label, current_value, value_type, max_length, multiline in test_cases:
-    result = prompt_value(label, current_value, value_type, max_length, multiline)
-    print(f"Result for '{label}': {result}")
+# data = {}
+# for name, value_type, max_length, multiline in test_cases:
+#     data[name] = prompt_value(name, value_type, max_length, multiline)
+
+# print()
+# print(data)
 
 import django
+from django import forms
+
 from django.conf import settings
 
 settings.configure(DEBUG=True, SECRET_KEY='ssh', ROOT_URLCONF=__name__)
 django.setup()
-
-from django import forms
 
 class PocForm(forms.Form):
     name = forms.CharField(max_length=16, label="What's your name?", help_text="Please enter your name.")
@@ -83,7 +56,14 @@ class PocForm(forms.Form):
         if self.cleaned_data.get("options", None) == "b":
             raise ValidationError("NEVER choose b!")
 
-# form = PocForm(data=dict(numb=999))
+def prompt_field(field):
+    input_type = getattr(field.widget, "input_type", None)
+    print(input_type)
+
+form = PocForm(data=dict(numb=999))
+for field in form.fields.values():
+    prompt_field(field)
+
 # form.is_valid()
 # field_value = prompt_field(form, 'numb')
 # print(field_value)
