922fc5c5def19321193c2c5da7216a84b4089bc7
Rune Kaagaard <rumi.kg@gmail.com>
2023-12-26 15:17:58 +0100

    Adding template mission

--- example_project/poc.py
+++ example_project/poc.py
@@ -92,6 +92,28 @@ def booking(*, state, inpt, first, **configuration):
 
     return command, state, concerns
 
+def template_text(title, description):
+    def _():
+        print(colored(title, attrs=["bold"]))
+        print()
+        print(description)
+        print()
+        direction_change, _ = prompt_value("Press enter to continue", required=False)
+        return direction_change, True
+
+    return _
+
+@mission(concerns=d(rendering=d(text=template_text)), configurator=None, pageno=lambda *a, **kw: (1, 1))
+def template(title, description, *, state, inpt, first, **configuration):
+    command = trek.RETRY
+    if inpt:
+        command = trek.CONTINUE
+
+    concerns = template.hypertrek["concerns"].copy()
+    concerns["rendering"] = {k: v(title, description, **configuration) for k, v in concerns["rendering"].items()}
+
+    return command, state, concerns
+
 class PocForm(forms.Form):
     name = forms.CharField(max_length=16, label="What's your name?", help_text="Please enter your name.")
     description = forms.CharField(widget=forms.Textarea, label="Write a few words about yourself ...",
@@ -109,10 +131,13 @@ class PocForm(forms.Form):
 
 def poc_trek():
     return [
+        template("This is the beginning", "Lets go!"),
         ms.form.form(PocForm, fields=["name", "description"]),
         ms.form.form(PocForm, fields=["options"]),
+        template("This is the middle", "Super great!"),
         booking(),
         ms.form.form(PocForm, fields=["num_int", "num_float"]),
+        template("This is the ending", "Finally!"),
     ]
 
 def log(inpt, state, command, i, direction_change):
