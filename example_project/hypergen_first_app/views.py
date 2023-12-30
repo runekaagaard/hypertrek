3b5bf94dc3603e0e8c6f7c1f8ffe49e32a989488
Rune Kaagaard <rumi.kg@gmail.com>
2023-12-30 17:57:46 +0100

    Progress bar.

--- example_project/hypergen_first_app/views.py
+++ example_project/hypergen_first_app/views.py
@@ -1,3 +1,4 @@
+d = dict
 from hypergen.imports import *
 
 from contextlib import contextmanager
@@ -25,6 +26,7 @@ base_template.target_id = "content"
 
 def trek_template(poc, state, concerns):
     min_, max_, current = trek.page_number(poc, state)
+    progress(value=current / ((max_+min_) / 2), style=d(width="100%"))
     p("On page ", current, " out of minimum ", min_, " and maximum ", max_, ".")
 
     inpt = concerns["rendering"]["hypergen"]()
